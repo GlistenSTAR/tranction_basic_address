6cc908731a0014a15a6c4623279e7028e8598db9
GlistenSTAR <glistenstar00@gmail.com>
2023-09-12 16:07:54 +0200

    feat: add function to find invest_address and amount which related mexc address

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const readline = require('linebyline')
 
 require('dotenv').config();
 
@@ -19,19 +20,23 @@ require('dotenv').config();
 //             data: {
 //                 "row": 100,
 //                 "page": page_num,
-//                 "address": process.env.MEXC_ADDRESS
+//                 "address": process.env.MEXC_ADDRESS,
+//                 "direction": "received"
 //             }
 //         }).then(function (res) {
 //             count = res.data.data.count
 //             data = res.data.data.transfers ? res.data.data.transfers : false;
 //         }).catch(function (error) {
 //             data = false
+//             console.log(error)
 //         });
 //         if (data) {
 //             data.map((item) => {
 //                 if (!related_address.find((el) => el == item.from)) {
-//                     related_address.push(item.from)
-//                     fs_address.push(item.from)
+//                     if (item.to == process.env.MEXC_ADDRESS) {
+//                         related_address.push(item.from)
+//                         fs_address.push(item.from)
+//                     }
 //                 }
 //             })
 //         } else {
@@ -42,9 +47,61 @@ require('dotenv').config();
 //         if (err) {
 //             console.error(err);
 //         }
+//         console.log('file saved')
 //     });
 // }
-
 // get_related_address()
+
+
+const result_data = require('./result.json')
+const invest_address = require('./invest_address.json')
+let finial_result = []
+let r = result_data.slice(0, 20).map(async (addr) => {
+    let count = 1;
+    await axios({
+        method: 'post',
+        url: "https://joystream.api.subscan.io/api/scan/transfers",
+        headers: {
+            "Content-Type": "application/json",
+            "X-API-Key": process.env.SUBSCAN_API,
+        },
+        data: {
+            "row": 1,
+            "address": addr,
+            "direction": "received"
+        }
+    }).then(function (res) {
+        count = res.data.data.count
+        console.log(count)
+    }).catch(function (error) {
+        console.log(error)
+        data = false
+    });
+    await axios({
+        method: 'post',
+        url: "https://joystream.api.subscan.io/api/scan/transfers",
+        headers: {
+            "Content-Type": "application/json",
+            "X-API-Key": process.env.SUBSCAN_API,
+        },
+        data: {
+            "row": count,
+            "address": addr,
+            "direction": "received"
+        }
+    }).then(function (res) {
+        data = res.data.data.transfers
+        data.forEach(element => {
+            if (invest_address.find((el) => el == element.from)) {
+                finial_result.push({ address: element.from, amount: element.amount })
+                console.log(element.from, element.to, element.amount)
+            }
+        });
+    }).catch(function (error) {
+        data = false
+    });
+    return true;
+})
+console.log(r)
 
 
